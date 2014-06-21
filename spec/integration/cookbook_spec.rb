785af61e9f0d2d59ec72f3d245f9d9e0cd44a79c
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-06-21 14:48:57 -0700

    Improve integration spec
    So it actually passes on Travis

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe 'sprout-chruby' do
   before :all do
-    expect(system('which chruby')).to eq(false)
+    expect(system('brew list chruby')).to eq(false)
     expect(system('soloist')).to eq(true)
   end
 
   it 'installs chruby' do
-    expect(system('which chruby')).to eq(true)
+    expect(system('brew list chruby')).to eq(true)
+  end
+
+  it 'installs a few rubies' do
+    expect(Dir.exist?(File.join(ENV['HOME'], '/.rubies'))).to eq(true)
+  end
+
+  it 'sets a default ruby' do
+    expect(File.exist?(File.join(ENV['HOME'], '/.ruby-version'))).to eq(true)
   end
 end
