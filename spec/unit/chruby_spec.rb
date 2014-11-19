84f45596c804f1988b123e094d05d558cb74ca04
aram price <me+git@aramprice.com>
2014-11-18 17:38:45 -0800

    Tidy up spec

--- spec/unit/chruby_spec.rb
+++ spec/unit/chruby_spec.rb
@@ -2,35 +2,38 @@ require 'unit/spec_helper'
 
 describe 'sprout-chruby::chruby' do
   let(:runner) { ChefSpec::SoloRunner.new }
+  let(:auto_change_ruby) { nil }
 
-  before { runner.converge(described_recipe) }
+  before do
+    runner.node.set['sprout']['chruby']['auto_change_ruby'] = auto_change_ruby
+
+    runner.converge(described_recipe)
+  end
 
   it 'installs the chruby package' do
     expect(runner).to install_package('chruby')
   end
 
   context 'when sprout.chruby.auto_change_ruby is true' do
-    before { runner.node.set['sprout']['chruby']['auto_change_ruby'] = true }
+    let(:auto_change_ruby) { true }
 
     it 'installs the bash-it plugin chruby-auto' do
-      runner.converge(described_recipe)
-
       expect(runner).to enable_bash_it_feature('plugins/chruby-auto')
+    end
+
+    it 'does not install the bash-it plugin chruby' do
+      expect(runner).not_to enable_bash_it_feature('plugins/chruby')
     end
   end
 
   context 'when sprout.chruby.auto_change_ruby is not true' do
-    before { runner.node.set['sprout']['chruby']['auto_change_ruby'] = false }
+    let(:auto_change_ruby) { false }
 
     it 'installs the bash-it plugin chruby' do
-      runner.converge(described_recipe)
-
       expect(runner).to enable_bash_it_feature('plugins/chruby')
     end
 
     it 'does not install the bash-it plugin chruby-auto' do
-      runner.converge(described_recipe)
-
       expect(runner).not_to enable_bash_it_feature('plugins/chruby-auto')
     end
   end
