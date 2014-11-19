16641b818bd0c306ffe88130dd9510dbe0f6b8cc
aram price <me+git@aramprice.com>
2014-11-18 16:02:44 -0800

    Make spec description more explicit

--- spec/unit/chruby_auto_spec.rb
+++ spec/unit/chruby_auto_spec.rb
@@ -3,7 +3,7 @@ require 'unit/spec_helper'
 describe 'sprout-chruby::chruby_auto' do
   let(:runner) { ChefSpec::Runner.new }
 
-  it 'installs chruby_auto' do
+  it 'installs the bash-it plugin chruby-auto' do
     runner.converge(described_recipe)
 
     expect(runner).to enable_bash_it_feature('plugins/chruby-auto')
