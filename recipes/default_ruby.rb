083c2bd71013ae5b6df762a3ea3443e3a442eae6
Abhijit Hiremagalur <pair+abhijit@pivotal.io>
2014-12-20 11:05:19 -0800

    Replace node['current_user'] with node['sprout']['user']
    In Chef 12 node['current_user'] is root under sudo, we want this to be the normal user (e.g. pivotal) so we're using ENV['SUDO_USER'] instead. Several recipes that manage file permissions rely on this.
    
    See https://github.com/pivotal-sprout/sprout-base/issues/3 for more info

--- recipes/default_ruby.rb
+++ recipes/default_ruby.rb
@@ -1,5 +1,5 @@
 file ::File.join(node['sprout']['home'], '.ruby-version') do
   content node['sprout']['chruby']['default_ruby']
-  user node['current_user']
+  user node['sprout']['user']
   action :create_if_missing
 end
--- recipes/rubies.rb
+++ recipes/rubies.rb
@@ -4,7 +4,7 @@ ruby_install_dir = ::File.join(node['sprout']['home'], '.rubies')
 node['sprout']['chruby']['rubies'].each do |ruby_vm, ruby_versions|
   ruby_versions.each do |ruby_version|
     execute "ruby-install #{ruby_vm} #{ruby_version}" do
-      user node['current_user']
+      user node['sprout']['user']
       not_if { ::Dir.exist?(::File.join(ruby_install_dir, [ruby_vm, ruby_version].join('-'))) }
     end
   end
